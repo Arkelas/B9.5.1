07edc6e8dfeda9fdb16d2bb12ca59fa4282034fd
Arkelas <breagon@mail.ru>
2022-07-14 08:41:08 +0000

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+var myarray = ['x', 'y', 'z']
+console.log('Length:');
+console.log(myarray.length);
+
+var first = myarray[0];
+var last = myarray[myarray.length - 1];
+console.log('First:');
+console.log(first);
+console.log('Last:');
+console.log(last);
+
+
+
